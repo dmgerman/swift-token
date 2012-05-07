485781d6d2c670d5c16cab7d20b5dcd29c7ccca3
John Dickinson <me@not.mn>
2012-05-07 09:41:42 -0500

    removed domain remap middleware
    The code has moved to https://github.com/notmyname/swift-domainremap.
    
    For current users of domain remap, this will require installing the new
    package and changing the "use" line of the domain remap conf section's
    to:
    
    [filter:domain_remap]
    use = egg:swift_domainremap#swift_domainremap
    
    And then 'swift-init proxy reload'.
    
    Change-Id: I710caf9b991f9d37df36b826ae4338086d0ec36d

--- setup.py
+++ setup.py
@@ -289,9 +289,6 @@ nl|'\n'
 string|"'catch_errors=swift.common.middleware.catch_errors:filter_factory'"
 op|','
 nl|'\n'
-string|"'domain_remap=swift.common.middleware.domain_remap:filter_factory'"
-op|','
-nl|'\n'
 string|"'swift3=swift.common.middleware.swift3:filter_factory'"
 op|','
 nl|'\n'

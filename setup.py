35b42c8d90d274b0edcb4ddc961bd9d241b1acd8
John Dickinson <me@not.mn>
2012-05-17 10:43:44 -0500

    Re-add cname lookup and domain remap middleware
    Revert "removed cname lookup middleware"
    
    This reverts commit b47bcf19e41e862ca84d77a7b8843f836e084b6a.
    
    Revert "removed domain remap middleware"
    
    This reverts commit 317cf868bdf66dbc17c02d4ca383efafa5e2f229.
    
    Change-Id: I260498d555c93b28896ace48a6f0e96701cbcc38

--- setup.py
+++ setup.py
@@ -295,6 +295,9 @@ nl|'\n'
 string|"'catch_errors=swift.common.middleware.catch_errors:filter_factory'"
 op|','
 nl|'\n'
+string|"'domain_remap=swift.common.middleware.domain_remap:filter_factory'"
+op|','
+nl|'\n'
 string|"'swift3=swift.common.middleware.swift3:filter_factory'"
 op|','
 nl|'\n'
